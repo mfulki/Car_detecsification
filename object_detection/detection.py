eb2b4254bc51b0246d438123cc0b09fd842fb48d
mfulki <muhammad.fulki@gmail.com>
2025-06-24 14:54:41 +0700

    feat:/add log

--- object_detection/detection.py
+++ object_detection/detection.py
@@ -1,6 +1,18 @@
+import sys
+import os
+import logging
+from datetime import datetime
 from ultralytics import YOLO
 
+os.makedirs("logs", exist_ok=True)
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+log_path = f"logs/train_{timestamp}.log"
 
+sys.stdout = open(log_path, 'w')
+sys.stderr = sys.stdout
+
+logging.basicConfig(level=logging.INFO)
+logging.info(f"Logging started at {timestamp}")
 
 if __name__ == "__main__":
     model = YOLO("yolo11n.pt")
@@ -22,8 +34,14 @@ if __name__ == "__main__":
 
     metrics = model.val()
 
+    logging.info(f"Training results: {train_results}")
+
     metrics = model.val()
+    logging.info(f"Validation metrics: {metrics}")
+
     export_path = model.export(format="onnx")
+    logging.info(f"Model exported to: {export_path}")
     
- 
+    sys.stdout.close()
+    
     
