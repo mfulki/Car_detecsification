fe40b522958b106acf39253f14fde07560e655c5
mfulki <muhammad.fulki@gmail.com>
2025-06-24 14:53:33 +0700

    feat:/wrap program for multiprocessing ex:gpu

--- object_detection/detection.py
+++ object_detection/detection.py
@@ -1,25 +1,29 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo11n.pt")
 
-train_results = model.train(
-    data="dataset/data.yaml",
-    epochs=60,             
-    imgsz=640,             
-    batch=16,              
-    device=0,            
-    workers=4,            
-    patience=10,          
-    project="runs/train",
-    name="vehicle",
-    save=True,
-    save_period=5,        
-    val=True   
-)
 
-metrics = model.val()
-metrics = model.val()
-export_path = model.export(format="onnx")
+if __name__ == "__main__":
+    model = YOLO("yolo11n.pt")
+    
+    train_results = model.train(
+        data="dataset/data.yaml",
+        epochs=60,             
+        imgsz=640,             
+        batch=16,              
+        device=0,            
+        workers=4,            
+        patience=10,          
+        project="runs/train",
+        name="vehicle",
+        save=True,
+        save_period=5,        
+        val=True   
+    )
+
+    metrics = model.val()
+
+    metrics = model.val()
+    export_path = model.export(format="onnx")
     
  
     
